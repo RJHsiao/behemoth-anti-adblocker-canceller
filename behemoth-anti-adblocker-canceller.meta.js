f445b6963ec6cce1c2f92f1f4310ef6a0b4f5dab
RJ Hsiao <rj19860725@gmail.com>
2016-04-13 18:27:01 +0800

    First commit

---
+++ behemoth-anti-adblocker-canceller.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name               Behemoth Anti-AdBlocker Canceller
+// @name:zh-TW         貝西摩斯廣告過濾反制消除器
+// @description        This userscript is used to cancel the anti-AdBlock mechanism in m.gamer.com.tw
+// @description:zh-TW  AdBlock無罪！反抗有理！貝西摩斯廣告過濾反制消除器幫你對抗巴哈姆特電玩資訊站手機版網站那太離譜太超過的廣告過濾反制機制，讓你無須停用AdBlock套件仍可正常瀏覽！
+// @namespace          http://rjhsiao.me/gmscripts
+// @version            1.0
+// @author             RJ Hsiao
+// @downloadURL        
+// @updateURL          
+// @supportURL         https://github.com/RJHsiao/behemoth-anti-adblocker-canceller
+// @license            gpl
+// @compatible         chrome
+// @match              http://m.gamer.com.tw/home/creationDetail.php?sn=*
+// @grant              none
+// ==/UserScript==
